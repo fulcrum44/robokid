3faa81aa581077ffc95968a2985babea68c36e4f
Joel <115259065+fulcrum44@users.noreply.github.com>
2026-04-01 13:22:24 +0200

    Revert "Commit con el logueado y el registro"

--- app/lib/screens/screens.dart
+++ app/lib/screens/screens.dart
@@ -1,3 +1,1 @@
-export 'package:robokid/screens/login_screen.dart';
-export 'package:robokid/screens/register_screen.dart';
-export 'package:robokid/screens/blocks_screen.dart';
+export 'package:robokid/screens/main_screen.dart';
--- app/lib/widgets/widgets.dart
+++
@@ -1,3 +0,0 @@
-export 'package:robokid/widgets/login_form_widget.dart';
-export 'package:robokid/widgets/custom_appbar.dart';
-export 'package:robokid/widgets/custom_snackbar.dart';
