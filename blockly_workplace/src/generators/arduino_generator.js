7f271ef309cad5db50c7a84709b19ed4c98265c3
Joel <115259065+fulcrum44@users.noreply.github.com>
2026-04-13 14:12:29 +0200

    Generacón código c++ de los bloques (#10)

---
+++ blockly_workplace/src/generators/arduino_generator.js
@@ -0,0 +1,28 @@
+import * as Blockly from 'blockly';
+
+export const arduinoGenerator = new Blockly.Generator('Arduino');
+
+arduinoGenerator.ORDER_ATOMIC = 0;
+arduinoGenerator.ORDER_NONE = 99;
+
+// instanciamos la variable para almacenar los includes o variables locales usadas
+arduinoGenerator.definitions_ = {};
+
+// variable para guardar la configuración de bloques que se haye hecho en el editor de bloques
+arduinoGenerator.setups_ = {};
+
+// ensamblado de los bloques en un sketch unificado
+arduinoGenerator.finish = function(code) {
+  const includes = Object.values(this.definitions_).join('\n');
+  const setups   = Object.values(this.setups_).join('\n  ');
+  return `${includes}
+
+void setup() {
+  ${setups}
+}
+
+void loop() {
+  ${code}
+}
+`;
+};
---
+++ blockly_workplace/src/generators/tiempo.js
@@ -0,0 +1,9 @@
+import { arduinoGenerator } from './arduino_generator.js';
+
+arduinoGenerator['esperar_segundos'] = function(block) {
+  const segundos = block.getFieldValue('SEGUNDOS');
+
+  // convertimos a milisegundos porque nativamente el delay() de arduino trabaja con milisegundos
+  const ms = Math.round(segundos * 1000);
+  return `delay(${ms});\n`;
+};
