7cadcc1508f380a7ceccfbf69caf8b9c75a7306a
Alejandro <200733732+Alejandro4587@users.noreply.github.com>
2026-04-13 10:16:12 +0200

    Proyecto robot daniel part2 - Añadido los bloques cambiar_velocidad, esperar_segundos y detectar_obstaculo (#9)

--- blockly_workplace/src/blocks/sensor.js
+++ blockly_workplace/src/blocks/sensor.js
@@ -2,6 +2,22 @@ export const leerDistancia = {
   "type": "leer_distancia",
   "message0": "Distancia del obstáculo (cm)",
   "output": "Number",
-  "colour": 190,
+  "colour": 760,
   "tooltip": "Mide a cuántos centímetros está el objeto más cercano, como un murciélago"
+};
+
+export const detectarObstaculo = {
+  "type": "detectar_obstaculo",
+  "message0": "¿Hay algo a menos de %1 cm?",
+  "args0": [
+    {
+      "type": "field_number",
+      "name": "DISTANCIA",
+      "value": 10,
+      "min": 0
+    }
+  ],
+  "output": "Boolean",
+  "colour": 150,
+  "tooltip": "Te avisa si el robot está a punto de chocar con algo"
 };
---
+++ blockly_workplace/src/blocks/tiempo.js
@@ -0,0 +1,16 @@
+export const esperarSegundos = {
+  "type": "esperar_segundos",
+  "message0": "Esperar %1 segundos",
+  "args0": [
+    {
+      "type": "field_number",
+      "name": "SEGUNDOS",
+      "value": 1,
+      "min": 0
+    }
+  ],
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 355,
+  "tooltip": "Hace que el robot se quede haciendo lo mismo o pausado durante este tiempo"
+}; 
