7cc47fb6e1b993a2aee465379489820ddf4044e6
Fulcrum <joelchicano70@gmail.com>
2026-04-29 22:00:14 +0200

    Wrapper creado para gestionar el estado de una sesión y hacerlas persistente independientemente de si la app está en ejecución

--- app/lib/screens/screens.dart
+++ app/lib/screens/screens.dart
@@ -4,3 +4,4 @@ export 'package:robokid/screens/blocks_user_screen.dart';
 export 'package:robokid/screens/record_screen.dart';
 export 'package:robokid/screens/navigation_screen.dart';
 export 'package:robokid/screens/ajustes_screen.dart';
+export 'package:robokid/screens/wrapper.dart';
