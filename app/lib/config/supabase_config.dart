26c40b5ffcde6c10dd41dc11b4a05c774abf063f
Fulcrum <joelchicano70@gmail.com>
2026-05-01 20:40:31 +0200

    configuracion supabase antigua borrada

--- app/lib/config/supabase_config.dart
+++
@@ -1,4 +0,0 @@
-class SupabaseConfig {
-  static const String urlSupabase = 'https://kgqmajghwcqfakhcsgfn.supabase.co'; // Url Base de datos Supabase
-  static const String anonKeySupabase = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtncW1hamdod2NxZmFraGNzZ2ZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1OTYyMTQsImV4cCI6MjA4OTE3MjIxNH0.mS4pzgkMtD25kSc2p0nmbL5Zl6O5j1d2gJ5WoNZ6KLI'; // 
-}
