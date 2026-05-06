cef23b91081bb56cd3e5e014415958f4a6d48d70
Joel <115259065+fulcrum44@users.noreply.github.com>
2026-05-06 12:48:08 +0200

    Rediseño código c++ bloques (#25)

--- blockly_workplace/src/generators/tiempo.js
+++
@@ -1,9 +0,0 @@
-import { arduinoGenerator } from './arduino_generator.js';
-
-arduinoGenerator.forBlock['esperar_segundos'] = function(block) {
-  const segundos = block.getFieldValue('SEGUNDOS');
-
-  // convertimos a milisegundos porque nativamente el delay() de arduino trabaja con milisegundos
-  const ms = Math.round(segundos * 1000);
-  return `delay(${ms});\n`;
-};
